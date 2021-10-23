473438de78088e2088e9144e0bfbbac34d16bcec
Manoel Vitor Brito Moura <manoelvitorbrito@gmail.com>
2021-10-23 01:31:51 -0300

    criando socket, código está comentado pois está dando erro ao utilizar o socket IO usando o esModules

---
+++ aulas/aula01/gdrive-webapi/src/index.mjs
@@ -0,0 +1,34 @@
+import https from "https";
+import fs from "fs";
+import { logger } from "./logger.mjs";
+import Routes from "./routes";
+// import { Server } from "socket.io";
+
+const PORT = process.env.PORT || 3000;
+
+const localHostSSL = {
+  key: fs.readFileSync("./certificates/key.pem"),
+  cert: fs.readFileSync("./certificates/cert.pem"),
+};
+
+const routes = new Routes();
+
+const server = https.createServer(localHostSSL, routes.handler.bind(routes));
+
+// const io = new Server(server, {
+//   cors: {
+//     origin: "*",
+//     credentials: false,
+//   },
+// });
+
+// routes.setSocketInstance(io);
+
+// io.on("connection", (sokect) => logger.info(`someone connected: ${sokect.id}`));
+
+const startServer = () => {
+  const { address, port } = server.address();
+  logger.info(`app running at https://${address}:${port}`);
+};
+
+server.listen(PORT, startServer);
