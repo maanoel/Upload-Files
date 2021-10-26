c0f27d99b45a04454d119589736a57e3a6b4f211
Manoel Vitor Brito Moura <manoelvitorbrito@gmail.com>
2021-10-26 19:39:26 -0300

    testando o cors com jest

--- aulas/aula01/gdrive-webapi/test/unit/routes.test.js
+++ aulas/aula01/gdrive-webapi/test/unit/routes.test.js
@@ -2,10 +2,6 @@ import { describe, test, expect, jest } from "@jest/globals";
 import Routes from "./../../src/routes.mjs";
 
 describe("#Routes test suite", () => {
-  describe("todo example", () => {
-    test.todo("example todo um jest");
-  });
-
   describe("#Routes test suite", () => {
     test("setSocket should store io instance", () => {
       const routes = new Routes();
@@ -43,13 +39,26 @@ describe("#Routes test suite", () => {
     test("given an inexistent route it should choose default route", () => {
       const routes = new Routes();
       const params = { ...defaultParams };
+
       params.request.method = "inexistent";
       routes.handler(...params.values());
 
       expect(params.response.end).toHaveBeenCalledWith("hello world");
     });
 
-    test.todo("it should set any request with CORS enabled");
+    test("it should set any request with CORS enabled", () => {
+      const routes = new Routes();
+      const params = { ...defaultParams };
+
+      params.request.method = "inexistent";
+      routes.handler(...params.values());
+
+      expect(params.response.setHeader).toHaveBeenCalledWith(
+        "Access-Control-Allow-Origin",
+        "*"
+      );
+    });
+
     test.todo("given method OPTIONS it should choose options route");
     test.todo("given method POST it should choose post route");
     test.todo("given method GET it should choose get route");
