f5ac96593ab4099b07777c68c2f94796cf8a4d91
Manoel Vitor Brito Moura <manoelvitorbrito@gmail.com>
2021-10-27 10:21:23 -0300

    um pouco de promises

--- aulas/aula01/gdrive-webapi/src/fileHelper.js
+++ aulas/aula01/gdrive-webapi/src/fileHelper.js
@@ -3,5 +3,9 @@ import fs from "fs";
 export default class FileHelper {
   static async getFilesStatus(downloadsFolder) {
     const currentFiles = await fs.promises.readdir(downloadsFolder);
+
+    await Promise.all(
+      currentFiles.map((file) => fs.promises.stat(downloadsFolder + "/" + file))
+    );
   }
 }
