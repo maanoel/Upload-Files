804d27f5f23bd00be37eab6b5a516f4009bf1476
Manoel Vitor Brito Moura <manoelvitorbrito@gmail.com>
2021-10-28 10:10:19 -0300

    adicionando arquivos

--- aulas/aula03/gdrive-webapi/test/integration/routes.test.js
+++ aulas/aula03/gdrive-webapi/test/integration/routes.test.js
@@ -1,78 +1,81 @@
 import {
-    describe,
-    test,
-    expect,
-    beforeAll,
-    afterAll,
-    jest
-} from '@jest/globals'
-import fs from 'fs'
-import FileHelper from '../../src/fileHelper.js'
+  describe,
+  test,
+  expect,
+  beforeAll,
+  afterAll,
+  jest,
+} from "@jest/globals";
+import fs from "fs";
+import FileHelper from "../../src/fileHelper.js";
 
-import Routes from './../../src/routes.js'
-import FormData from 'form-data'
-import TestUtil from '../_util/testUtil.js'
-import { logger } from '../../src/logger.js'
-import { tmpdir } from 'os'
-import { join } from 'path'
+import Routes from "./../../src/routes.js";
+import FormData from "form-data";
+import TestUtil from "../_util/testUtil.js";
+import { logger } from "../../src/logger.js";
+import { tmpdir } from "os";
+import { join } from "path";
 
-describe('#Routes Integration Test', () => {
-    let defaultDownloadsFolder = ''
-    beforeAll(async () => {
-        defaultDownloadsFolder = await fs.promises.mkdtemp(join(tmpdir(), 'downloads-'))
-    })
+describe("#Routes Integration Test", () => {
+  let defaultDownloadsFolder = "";
+  beforeAll(async () => {
+    defaultDownloadsFolder = await fs.promises.mkdtemp(
+      join(tmpdir(), "downloads-")
+    );
+  });
 
-    afterAll(async () => {
-        await fs.promises.rm(defaultDownloadsFolder, { recursive: true })
-    })
+  afterAll(async () => {
+    await fs.promises.rm(defaultDownloadsFolder, { recursive: true });
+  });
 
-    beforeEach(() => {
-        jest.spyOn(logger, 'info')
-            .mockImplementation()
-    })
+  beforeEach(() => {
+    jest.spyOn(logger, "info").mockImplementation();
+  });
 
-    describe('#getFileStatus', () => {
-        const ioObj = {
-            to: (id) => ioObj,
-            emit: (event, message) => { }
-        }
+  describe("#getFilesStatus", () => {
+    const ioObj = {
+      to: (id) => ioObj,
+      emit: (event, message) => {},
+    };
 
-        test('should upload file to the folder', async () => {
-            const filename = 'semanajsexpert.png'
-            const fileStream = fs.createReadStream(`./test/integration/mocks/${filename}`)
-            const response = TestUtil.generateWritableStream(() => { })
+    test("should upload file to the folder", async () => {
+      const filename = "semanajsexpert.png";
+      const fileStream = fs.createReadStream(
+        `./test/integration/mocks/${filename}`
+      );
+      const response = TestUtil.generateWritableStream(() => {});
 
-            const form = new FormData()
-            form.append('photo', fileStream)
+      const form = new FormData();
+      form.append("photo", fileStream);
 
+      const defaultParams = {
+        request: Object.assign(form, {
+          headers: form.getHeaders(),
+          method: "POST",
+          url: "?socketId=10",
+        }),
 
-            const defaultParams = {
-                request: Object.assign(form, {
-                    headers: form.getHeaders(),
-                    method: 'POST',
-                    url: '?socketId=10'
-                }),
+        response: Object.assign(response, {
+          setHeader: jest.fn(),
+          writeHead: jest.fn(),
+          end: jest.fn(),
+        }),
+        values: () => Object.values(defaultParams),
+      };
 
-                response: Object.assign(response, {
-                    setHeader: jest.fn(),
-                    writeHead: jest.fn(),
-                    end: jest.fn()
-                }),
-                values: () => Object.values(defaultParams)
-            }
+      const routes = new Routes(defaultDownloadsFolder);
+      routes.setSocketInstance(ioObj);
+      const dirBeforeRan = await fs.promises.readdir(defaultDownloadsFolder);
+      expect(dirBeforeRan).toEqual([]);
+      await routes.handler(...defaultParams.values());
+      const dirAfterRan = await fs.promises.readdir(defaultDownloadsFolder);
+      expect(dirAfterRan).toEqual([filename]);
 
-            const routes = new Routes(defaultDownloadsFolder)
-            routes.setSocketInstance(ioObj)
-            const dirBeforeRan = await fs.promises.readdir(defaultDownloadsFolder)
-            expect(dirBeforeRan).toEqual([])
-            await routes.handler(...defaultParams.values())
-            const dirAfterRan = await fs.promises.readdir(defaultDownloadsFolder)
-            expect(dirAfterRan).toEqual([filename])
-
-            expect(defaultParams.response.writeHead).toHaveBeenCalledWith(200)
-            const expectedResult = JSON.stringify({ result: 'Files uploaded with success! ' })
-            expect(defaultParams.response.end).toHaveBeenCalledWith(expectedResult)
-
-        })
-    })
-})
+      expect(defaultParams.response.writeHead).toHaveBeenCalledWith(200);
+      const expectedResult = JSON.stringify({
+        result: "Files uploaded with success! ",
+      });
+      expect(defaultParams.response.end).toHaveBeenCalledWith(expectedResult);
+    });
+  });
+});
